00c3a42590005d9b099c44168ac4427b4db23833
Kulesko <dawid.kuleczko@gmail.com>
2020-08-02 20:41:52 +0200

    Initial commit

---
+++ src/Home.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+import Clock from 'react-live-clock';
+import Timer from './Timer';
+
+function Home() {
+  return (
+    <div className="App">
+      <header className="App-header">
+          <div className='card'>
+              <h1><Clock format={'HH:mm:ss'} ticking={true} timezone={'Europe/Oslo'} /></h1>
+            </div>
+            <div className='card'><Timer/></div>
+      </header>
+    </div>
+  );
+}
+
+export default Home;
