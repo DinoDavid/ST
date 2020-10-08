aec6d086a3d363cb46db74dda93c1ed8f75d7f3d
Kulesko <dawid.kuleczko@gmail.com>
2020-10-08 14:55:01 +0200

    Fixed Calendar and implemented analog clock

--- src/Home.jsx
+++ src/Home.jsx
@@ -2,15 +2,18 @@ import React from 'react';
 import './App.css';
 import Clock from 'react-live-clock';
 import Timer from './Timer';
+import MyCalendar from './MyCalender';
+import AnalogClock from './AnalogClock';
 
 function Home() {
   return (
     <div className="App">
       <header className="App-header">
-          <div className='card'>
-              <h1><Clock format={'HH:mm:ss'} ticking={true} timezone={'Europe/Oslo'} /></h1>
-            </div>
-            <div className='card'><Timer/></div>
+      <div className='cardBig'><MyCalendar/></div>
+      <div className='card'><h1><Clock format={'HH:mm:ss'} ticking={true} timezone={'Europe/Oslo'} /></h1></div>
+      <div className='card'><Timer/></div>
+      <div><AnalogClock/></div>
+
       </header>
     </div>
   );
